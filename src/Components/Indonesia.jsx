88baf80929aa5fb96ebc5ae3bb48db52428e2b6d
luthfika03 <luthfikaanggit2366@gmail.com>
2022-11-03 13:22:38 +0700

    tambahan

--- src/Components/Indonesia.jsx
+++ src/Components/Indonesia.jsx
@@ -8,6 +8,8 @@ const { Meta } = Card;
 
 function Indonesia() {
   const [articles, setArticles] = useState([]);
+  const [input, setInput] = useState('')
+  const [output, setOutput] = useState([])
 
   useEffect(() => {
     const getArticles = async () => {
@@ -19,30 +21,77 @@ function Indonesia() {
     };
     getArticles();
   }, []);
-
   console.log('articles', articles);
+
+  useEffect(() => {
+    setOutput([])
+    articles.filter(val => {
+      if (val.title.toLowerCase().includes(input.toLowerCase())) {
+        setOutput(output => [...output, val])
+      }
+    })
+  }, [input])
+
   return (
     <div className="news">
-      {articles &&
-        articles.map((article, index) => {
-          return (
-            <Card key={index}>
-              <h3 className="news__title">{article.title}</h3>
-              <p className="news__desc">{article.description}</p>
-              <span className="news__author">{article.author}</span> <br />
-              <span className="news__published">{article.publishedAt}</span>
-              <span className="news__source">{article.source.name}</span>
-              <div>
-                <a href={article.url} target="_blank" rel="noopener noreferrer">
-                  <Button className="btn btn-info me-1">News Page</Button>
-                </a>
-                <Button className="btn btn-primary">
-                  <span>Save</span>
-                </Button>
-              </div>
-            </Card>
-          );
-        })}
+      <div className='header'>
+        <h1>Indonesia News</h1>
+        <hr></hr>
+      </div>
+
+      
+      <div className='search'>
+        <div className='search-bar'>
+          <input onChange={e => setInput(e.target.value)}
+            type='text' placeholder="search"></input>
+        </div>
+
+        <div className='output'>
+          {output &&
+            output.map((article, index) => {
+              return (
+                <Card key={index}>
+                  <h3 className="news__title">{article.title}</h3>
+                  <p className="news__desc">{article.description}</p>
+                  <span className="news__author">{article.author}</span> <br />
+                  <span className="news__published">{article.publishedAt}</span>
+                  <span className="news__source">{article.source.name}</span>
+                  <div>
+                    <a href={article.url} target="_blank" rel="noopener noreferrer">
+                      <Button className="btn btn-info me-1">News Page</Button>
+                    </a>
+                    <Button className="btn btn-primary">
+                      <span>Save</span>
+                    </Button>
+                  </div>
+                </Card>
+              );
+            })}
+        </div>
+
+        <div className='indonesia-news'>
+          {articles &&
+            articles.map((article, index) => {
+              return (
+                <Card key={index}>
+                  <h3 className="news__title">{article.title}</h3>
+                  <p className="news__desc">{article.description}</p>
+                  <span className="news__author">{article.author}</span> <br />
+                  <span className="news__published">{article.publishedAt}</span>
+                  <span className="news__source">{article.source.name}</span>
+                  <div>
+                    <a href={article.url} target="_blank" rel="noopener noreferrer">
+                      <Button className="btn btn-info me-1">News Page</Button>
+                    </a>
+                    <Button className="btn btn-primary">
+                      <span>Save</span>
+                    </Button>
+                  </div>
+                </Card>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 }
